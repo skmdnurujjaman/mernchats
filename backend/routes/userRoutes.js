ced52a458c38c4b72feb722026df9a25d05d2d6c
skmdnurujjaman <smnideal@gmail.com>
2022-04-21 23:01:28 +0530

    Auth Done

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+router.route("/").get(protect, allUsers);
+router.route("/register").post(registerUser);
+router.route("/login").post(authUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-const app = express();
 dotenv.config();
+const app = express();
+connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.map((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`running on ${PORT}`);
